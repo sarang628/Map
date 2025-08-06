e34ce76d3e317780116b695616ec07b37152743e
sryang <sarang628@gmail.com>
2025-08-06 13:45:45 +0800

    move MapUiState to viewmodel

--- library/src/main/java/com/example/screen_map/uistate/MapUiState.kt
+++ library/src/main/java/com/example/screen_map/uistate/MapUiState.kt
@@ -1,8 +1,2 @@
 package com.example.screen_map.uistate
 
-import com.example.screen_map.data.MarkerData
-
-data class MapUiState(
-    val currentPosition: Int,
-    val selectedMarker: MarkerData? = null
-)
