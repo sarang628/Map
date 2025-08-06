1c86986eb3c3a8589f8038b7468b80949a753cf7
sryang <sarang628@gmail.com>
2025-08-06 13:51:15 +0800

    add uistate
    - add marker list in viewmodel

--- library/src/main/java/com/example/screen_map/viewmodels/MapViewModel.kt
+++ library/src/main/java/com/example/screen_map/viewmodels/MapViewModel.kt
@@ -3,24 +3,38 @@ package com.example.screen_map.viewmodels
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.screen_map.uistate.MapUiState
+import androidx.lifecycle.viewModelScope
+import com.example.screen_map.data.MarkerData
+import com.example.screen_map.usecase.GetMarkerListFlowUseCase
+import com.example.screen_map.usecase.GetMarkerListUseCase
 import com.example.screen_map.usecase.SavePositionUseCase
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor(
-    private val saveMapPositionUseCase: SavePositionUseCase
+    private val saveMapPositionUseCase: SavePositionUseCase,
+    private val getMarkerListFlowUseCase: GetMarkerListFlowUseCase
 ) : ViewModel() {
-    var isMapLoaded = MutableStateFlow(false)
+    var uiState: MapUIstate by mutableStateOf(MapUIstate(list = listOf()))
+        private set
+
+    init {
+        viewModelScope.launch {
+            try {
+                getMarkerListFlowUseCase.invoke().collect {
+                    uiState = uiState.copy(list = it)
+                }
+            }catch (e : Exception){
+                Log.e("__MapViewModel", e.toString())
+            }
+        }
+    }
 
     fun saveCameraPosition(state: CameraPositionState) {
         saveMapPositionUseCase.save(position = state.position)
@@ -38,12 +52,13 @@ class MapViewModel @Inject constructor(
     }
 
     fun onMapLoaded() {
-        isMapLoaded.update { true }
+        uiState = uiState.copy(isMapLoaded = true)
     }
-
-
-    val mapUiStateFlow = MutableStateFlow(
-        MapUiState(currentPosition = 0)
-    )
-
 }
+
+data class MapUIstate(
+    val list: List<MarkerData>,
+    val isMapLoaded: Boolean = false,
+    val currentPosition: Int = 0,
+    val selectedMarker: MarkerData? = null
+)
