b96e61c19ef513bb913e8a0f463b5d354f527d40
sryang <sarang628@gmail.com>
2025-08-06 13:48:34 +0800

    arrange code

--- library/src/main/java/com/example/screen_map/compose/MapScreenForFinding.kt
+++ library/src/main/java/com/example/screen_map/compose/MapScreenForFinding.kt
@@ -1,26 +1,11 @@
 package com.example.screen_map.compose
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import android.util.Log
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.screen_map.data.MarkerData
 import com.example.screen_map.data.icon
@@ -30,9 +15,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.Circle
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberMarkerState
 import kotlinx.coroutines.delay
@@ -50,16 +32,7 @@ import kotlinx.coroutines.launch
  * @param boundary 내 위치 반경 표시
  */
 @Composable
-fun MapScreenForFinding(
-    mapViewModel: MapViewModel = hiltViewModel(),
-    onMark: ((Int) -> Unit) = {},
-    cameraSpeed: Int = 300,
-    cameraPositionState: CameraPositionState,
-    list: List<MarkerData>?,
-    selectedMarkerData: MarkerData?,
-    onMapClick: (LatLng) -> Unit = {},
-    myLocation: LatLng? = null,
-    boundary: Double? = null,
+fun MapScreenForFinding(mapViewModel: MapViewModel = hiltViewModel(), onMark: ((Int) -> Unit) = {}, cameraSpeed: Int = 300, cameraPositionState: CameraPositionState, selectedMarkerData: MarkerData?, onMapClick: (LatLng) -> Unit = {}, myLocation: LatLng? = null, boundary: Double? = null,
 ) {
     val selectedMarker = rememberMarkerState().apply { showInfoWindow() }
     val isMapLoaded by mapViewModel.isMapLoaded.collectAsState()
@@ -73,10 +46,7 @@ fun MapScreenForFinding(
         //카드가 포커스된 음식점에 맞춰 지도 이동시키기
         selectedMarkerData?.let {
             if (selectedMarker.position != it.getLatLng()) {
-                cameraPositionState.animate(
-                    update = CameraUpdateFactory.newLatLng(it.getLatLng()),
-                    durationMs = cameraSpeed
-                )
+                cameraPositionState.animate(update = CameraUpdateFactory.newLatLng(it.getLatLng()), durationMs = cameraSpeed)
             }
         }
     }
@@ -87,47 +57,22 @@ fun MapScreenForFinding(
         selectedMarkerData = selectedMarkerData,
         onMapClick = onMapClick,
         onMark = onMark,
-        onMapLoaded = {
-            coroutine.launch {
-                if (!isMapLoaded) { // 플래그 처리 안하면 지도화면으로 이동할때마다 이벤트 발생 처음에 한번만 동작하면 됨
-                    cameraPositionState.animate(
-                        CameraUpdateFactory.newLatLngZoom(
-                            mapViewModel.getLastPosition(),
-                            mapViewModel.getLastZoom()
-                        ),
-                        durationMs = 1000
-                    )
-                    //카메라 이동 전까지 플래그 비활성화
-                    delay(1000)
-                    mapViewModel.onMapLoaded()
-                }
-            }
-        }
-    ){
-        list?.let {
+        onMapLoaded = { coroutine.launch {
+            if (!isMapLoaded) { // 플래그 처리 안하면 지도화면으로 이동할때마다 이벤트 발생 처음에 한번만 동작하면 됨
+                cameraPositionState.animate(CameraUpdateFactory.newLatLngZoom(mapViewModel.getLastPosition(), mapViewModel.getLastZoom()), durationMs = 1000)
+                delay(1000) //카메라 이동 전까지 플래그 비활성화
+                mapViewModel.onMapLoaded() }
+        }}) {
+        mapViewModel.uiState.list.let {
+            Log.d("__MapScreenForFinding", "markerSize : ${it.size}")
             for (data: MarkerData in it) {
-                Marker(
-                    state = data.markState(),
-                    title = data.title,
-                    snippet = data.snippet,
-                    onClick = {
-                        onMark?.invoke(Integer.parseInt(it.tag.toString()))
-                        false
-                    },
-                    tag = data.id,
-                    icon = BitmapDescriptorFactory.fromResource(data.icon)
-                )
+                Marker(tag = data.id, state = data.markState(), title = data.title, snippet = data.snippet, onClick = { onMark.invoke(Integer.parseInt(it.tag.toString())); false }, icon = BitmapDescriptorFactory.fromResource(data.icon))
             }
         }
 
         myLocation?.let { latlng ->
             boundary?.let {
-                Circle(
-                    center = latlng,
-                    radius = boundary,
-                    strokeWidth = 5f,
-                    strokeColor = MaterialTheme.colorScheme.primary
-                )
+                Circle(center = latlng, radius = boundary, strokeWidth = 5f, strokeColor = MaterialTheme.colorScheme.primary)
             }
         }
     }
