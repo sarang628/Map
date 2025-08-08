66e5d2e11401adf0f088589ba294b2d7c08b2b9e
sryang <sarang628@gmail.com>
2025-08-08 16:21:16 +0800

    add interact with list and marker

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -8,19 +8,27 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -35,6 +43,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.sarang.torang.data.Filter
+import com.sarang.torang.di.repository.repository.impl.FindRepositoryImpl
 import com.sarang.torang.repository.FindRepository
 import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -45,7 +54,7 @@ import kotlin.random.Random
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject lateinit var findRepository : FindRepository
+    @Inject lateinit var findRepository : FindRepositoryImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,11 +68,11 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalLayoutApi::class)
     @Composable
     fun MapTest(){
         val coroutineScope = rememberCoroutineScope()
         val cameraPositionState = rememberCameraPositionState()
-        var selectedMarkerData: MarkerData? by remember { mutableStateOf(null) }
         val list = testMarkArrayList()
         var location: Location? by remember { mutableStateOf(null) }
         var isMyLocationEnabled by remember { mutableStateOf(false) }
@@ -71,36 +80,49 @@ class MainActivity : ComponentActivity() {
         val navHostController = rememberNavController()
         var boundary by remember { mutableStateOf(0.0) }
         val mapViewModel: MapViewModel = hiltViewModel()
+        val restaurants = findRepository.restaurants.collectAsState().value
 
-        Box {
+        Scaffold {
+            Box(Modifier.padding(it)){
             NavHost(navController = navHostController, startDestination = "map") {
                 composable("map") {
                     //MapScreen(mapViewModel = mapViewModel, cameraPositionState = cameraPositionState, selectedMarkerData = selectedMarkerData)
-                    MapScreenForFinding(mapViewModel = mapViewModel, cameraPositionState = cameraPositionState, selectedMarkerData = selectedMarkerData)
+                    MapScreenForFinding(mapViewModel = mapViewModel, cameraPositionState = cameraPositionState)
                 }
                 composable("restaurant") {}
             }
 
-            Column(Modifier.scrollable(rememberScrollState(), orientation = Orientation.Horizontal)) {
-                Button(onClick = { selectedMarkerData = list[Random.Default.nextInt(list.size - 1)] }) {
-                    Text(text = "pageChange") }
-                Button(onClick = { coroutineScope.launch { cameraPositionState.animate(CameraUpdateFactory.zoomIn()) } }) {
-                    Text(text = "+") }
-                Button(onClick = { coroutineScope.launch { cameraPositionState.animate(CameraUpdateFactory.zoomOut()) } }) {
-                    Text(text = "-") }
-                CurrentLocationScreen(onLocation = {
-                    location = it
-                    isMyLocationEnabled = !isMyLocationEnabled; coroutineScope.launch {
-                    cameraPositionState.animate(update = CameraUpdateFactory.newLatLng(LatLng(it.latitude, it.longitude)), 300)
-                    myLocation = LatLng(it.latitude, it.longitude) } })
-                Button(onClick = { navHostController.navigate("restaurant") }) { Text(text = "aa") }
-                Button(onClick = { boundary = 100.0 }) { Text(text = "100M") }
-                Button(onClick = { boundary = 200.0 }) { Text(text = "200M") }
-                Button(onClick = { boundary = 500.0 }) { Text(text = "500M") }
-                Button(onClick = { boundary = 1000.0 }) { Text(text = "1000M") }
-                Button(onClick = { boundary = 3000.0 }) { Text(text = "3000M") }
-                Button(onClick = { coroutineScope.launch { findRepository.search(Filter()) }}) { Text(text = "filter") }
+            Column {
+                FlowRow(Modifier.scrollable(rememberScrollState(), orientation = Orientation.Horizontal)) {
+                    Button(onClick = { coroutineScope.launch { cameraPositionState.animate(CameraUpdateFactory.zoomIn()) } }) {
+                        Text(text = "+") }
+                    Button(onClick = { coroutineScope.launch { cameraPositionState.animate(CameraUpdateFactory.zoomOut()) } }) {
+                        Text(text = "-") }
+                    CurrentLocationScreen(onLocation = {
+                        location = it
+                        isMyLocationEnabled = !isMyLocationEnabled; coroutineScope.launch {
+                        cameraPositionState.animate(update = CameraUpdateFactory.newLatLng(LatLng(it.latitude, it.longitude)), 300)
+                        myLocation = LatLng(it.latitude, it.longitude) } })
+                    Button(onClick = { navHostController.navigate("restaurant") }) { Text(text = "aa") }
+                    Button(onClick = { boundary = 100.0 }) { Text(text = "100M") }
+                    Button(onClick = { boundary = 200.0 }) { Text(text = "200M") }
+                    Button(onClick = { boundary = 500.0 }) { Text(text = "500M") }
+                    Button(onClick = { boundary = 1000.0 }) { Text(text = "1000M") }
+                    Button(onClick = { boundary = 3000.0 }) { Text(text = "3000M") }
+                    Button(onClick = { coroutineScope.launch { findRepository.search(Filter()) }}) { Text(text = "filter") }
+                }
+                LazyColumn {
+                    items(restaurants.size){
+                        TextButton({
+                            coroutineScope.launch {
+                                findRepository.selectRestaurant(restaurants[it].restaurantId ?: -1)
+                            }
+                        }) {
+                            Text(restaurants[it].restaurantName.toString())
+                        }
+                    }
+                }
             }
         }
-    }
+    }}
 }
