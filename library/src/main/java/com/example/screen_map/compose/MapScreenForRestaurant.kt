121a7d4b978f1130db4c51ec58de9a85af7169bf
sryang <sarang628@gmail.com>
2025-09-05 11:10:23 +0800

    for single restaurant marker

--- library/src/main/java/com/example/screen_map/compose/MapScreenForRestaurant.kt
+++ library/src/main/java/com/example/screen_map/compose/MapScreenForRestaurant.kt
@@ -1,8 +1,7 @@
 package com.example.screen_map.compose
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -29,23 +28,36 @@ import kotlinx.coroutines.launch
  * @param boundary 내 위치 반경 표시
  */
 @Composable
-fun MapScreenForRestaurant(mapViewModel: MapViewModel = hiltViewModel(), cameraPositionState: CameraPositionState = rememberCameraPositionState(), selectedMarkerData: MarkerData?, mapUiSettings: MapUiSettings = MapUiSettings(zoomControlsEnabled = true), onMapClick: (LatLng) -> Unit = {}, zoom : Float, logoBottomPadding : Dp = 0.dp,) {
+fun MapScreenForRestaurant(mapViewModel         : MapViewModel = hiltViewModel(),
+                           restaurantId         : Int = -1,
+                           cameraPositionState  : CameraPositionState = rememberCameraPositionState(),
+                           selectedMarkerData   : MarkerData?,
+                           mapUiSettings        : MapUiSettings = MapUiSettings(zoomControlsEnabled = true),
+                           onMapClick           : (LatLng) -> Unit = {},
+                           zoom                 : Float = 0f,
+                           logoBottomPadding    : Dp = 0.dp) {
     val isMapLoaded = mapViewModel.uiState.isMapLoaded
     val coroutine = rememberCoroutineScope()
-    MapScreen(
-        mapViewModel = mapViewModel,
-        cameraPositionState = cameraPositionState,
-        onMapClick = onMapClick,
-        uiSettings = mapUiSettings,
-        logoBottomPadding = logoBottomPadding,
-        onMapLoaded = {
-            coroutine.launch {
-                if (!isMapLoaded) { // 플래그 처리 안하면 지도화면으로 이동할때마다 이벤트 발생 처음에 한번만 동작하면 됨
-                    cameraPositionState.animate(CameraUpdateFactory.newLatLngZoom(LatLng(selectedMarkerData?.lat ?: 0.0, selectedMarkerData?.lon ?: 0.0), zoom), durationMs = 1000)
-                    delay(1000)//카메라 이동 전까지 플래그 비활성화
-                    mapViewModel.onMapLoaded()
+    //TODO:: restaurantId를 조회하여 마커 표시하기
+    if(restaurantId > 0) {
+        MapScreen(
+            mapViewModel = mapViewModel,
+            cameraPositionState = cameraPositionState,
+            onMapClick = onMapClick,
+            uiSettings = mapUiSettings,
+            logoBottomPadding = logoBottomPadding,
+            onMapLoaded = {
+                coroutine.launch {
+                    if (!isMapLoaded) { // 플래그 처리 안하면 지도화면으로 이동할때마다 이벤트 발생 처음에 한번만 동작하면 됨
+                        cameraPositionState.animate(CameraUpdateFactory.newLatLngZoom(LatLng(selectedMarkerData?.lat ?: 0.0, selectedMarkerData?.lon ?: 0.0), zoom), durationMs = 1000)
+                        delay(1000)//카메라 이동 전까지 플래그 비활성화
+                        mapViewModel.onMapLoaded()
+                    }
                 }
             }
-        }
-    )
+        )
+    }
+    else {
+        Text("잘못된 음식점 id. $restaurantId")
+    }
 }
